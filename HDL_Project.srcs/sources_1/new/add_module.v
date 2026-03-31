b9b95c461137d126da431f915dc417c24799a933
James Laurence <jameslaurencebelen123@gmail.com>
2026-03-31 15:51:18 +0800

    added overflow falg for add and sub

--- HDL_Project.srcs/sources_1/new/add_module.v
+++ HDL_Project.srcs/sources_1/new/add_module.v
@@ -21,16 +21,17 @@
 
 
     module add_module #(parameter N = 8)(
-           c, s, x, y
+           c, s, o, x, y
         );
         input [N-1:0] x, y;
         output reg [N-1:0] s;
-        output reg c;
+        output reg c, o;
         reg [N:0] sum;
         
         always @(x, y) begin
             sum = x + y;
             s = sum[N-1:0];
             c = sum[N];
+            o = (x[N-1] == y[N-1]) && (s[N-1] != x[N-1]);
         end
     endmodule
--- HDL_Project.srcs/sources_1/new/subtract_module.v
+++ HDL_Project.srcs/sources_1/new/subtract_module.v
@@ -21,16 +21,17 @@
 
 
 module subtract_module #(parameter N = 8)(
-        b, d, x, y
+        b, d, o, x, y
     );
     input [N-1:0] x, y;
     output reg [N-1:0] d;
-    output reg b;
+    output reg b, o;
     reg [N:0] diff;
     
     always @(x, y) begin
         diff = x - y;
         d = diff[N-1:0];
         b = ~diff[N];
+        o = (x[N-1] != y[N-1]) && (d[N-1] != x[N-1]);
     end
 endmodule
